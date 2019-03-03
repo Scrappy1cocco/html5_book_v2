1a5b291e8d9113194b5e3111c2b3e11f3fda5c92
Scrappy <jedediah.shine100@gmail.com>
2019-03-03 22:00:52 +0300

    localStorage done

--- localStorage/task1/task1.js
+++ localStorage/task1/task1.js
@@ -3,12 +3,101 @@
 
 window.onload = init; 
 
-var button = document.getElementById('add_button');
-button.onclick = createSticky;
+function init() {
+
+	var button = document.getElementById('add_button');
+	button.onclick = createSticky;
+
+	var stickiesArray = getStickiesArray();
+
+	for (var i = 0; i < stickiesArray.length; i++) {
+		var key = stickiesArray[i];
+		var value = JSON.parse(localStorage[key]);
+		addStickyToDOM(key, value);
+	}
+}
+
+function addStickyToDOM(key, stickyObj) {
+	var stickies = document.getElementById('stickies');
+	var sticky = document.createElement('li');
+	sticky.setAttribute("id", key);
+
+	sticky.style.backgroundColor = stickyObj.color;
+
+	var span = document.createElement('span');
+	span.setAttribute('class', 'sticky');
+
+	span.innerHTML = stickyObj.value;
+	sticky.appendChild(span);
+	stickies.appendChild(sticky);
+	sticky.onclick = deleteSticky;
+}
+
+function createSticky() {
+	var stickiesArray = getStickiesArray();
+	var currentDate = new Date();
+	var colorSelectObj = document.getElementById("note_color");
+	var index = colorSelectObj.selectedIndex;
+	var color = colorSelectObj[index].value;
+	var key = "sticky_" + currentDate.getTime();
+	var value = document.getElementById('note_text').value;
+	var stickyObj = {
+		"value": value,
+		"color": color
+	};
+	localStorage.setItem(key, JSON.stringify(stickyObj));
+	stickiesArray.push(key)
+	localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	addStickyToDOM(key, stickyObj);
+}
+
+function getStickiesArray() {
+
+	var stickiesArray = localStorage["stickiesArray"];
+	if (!stickiesArray) {
+		stickiesArray = [];
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	} else {
+		stickiesArray = JSON.parse(stickiesArray);
+	}
+
+	return stickiesArray;
+}
+
+function deleteSticky(e) {
+	var key = e.target.id;
+	if (e.target.tagName.toLowerCase() == "span") {
+		key = e.target.parentNode.id;
+	}
+	localStorage.removeItem(key);
+	var stickiesArray =  getStickiesArray();
+	if (stickiesArray) {
+		for (var i = 0; i < stickiesArray.length; i++) {
+			if (key == stickiesArray[i]) {
+				stickiesArray.splice(i, 1);
+			}
+		}
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+		removeStickyFromDOM(key);
+	}
+}
+
+function removeStickyFromDOM(key) {
+	var sticky = document.getElementById(key);
+	sticky.parentNode.removeChild(sticky);
+}
 
 var clearButton = document.getElementById('clear_button');
+
 clearButton.onclick = function() {
-	localStorage.clear();
+	var stickiesArray = getStickiesArray();
+	for (var i = 0; i < stickiesArray.length; i++) {
+		var key = stickiesArray[i];
+		localStorage.removeItem(key);
+	}
+	stickiesArray = [];
+	localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	localStorage.removeItem("stickiesArray");
 	var stickies = document.getElementById('stickies');
 	var lis = document.getElementsByTagName('li');
 	var currentLength = lis.length
@@ -16,33 +105,4 @@ clearButton.onclick = function() {
 	for (var i = 0; i < currentLength; i++) {
 		stickies.removeChild(lis[lis.length - 1]);
 	}
-}
-
-function init() {
-	for (var i = 0; i < localStorage.length; i++) {
-		var key = localStorage.key(i);
-		if (key.substring(0, 6) == "sticky") {
-			var value = localStorage.getItem(key);
-			addStickyToDOM(value);
-		}
-	}
-}
-
-function addStickyToDOM(value) {
-	var stickies = document.getElementById('stickies');
-	var sticky = document.createElement('li');
-	var span = document.createElement('span');
-	span.setAttribute('class', 'sticky');
-
-	span.innerHTML = value;
-	sticky.appendChild(span);
-	stickies.appendChild(sticky);
-}
-
-function createSticky() {
-	var value = document.getElementById('note_text').value;
-	var key = "sticky_" + localStorage.length;
-	localStorage.setItem(key, value);
-
-	addStickyToDOM(value);
 }
